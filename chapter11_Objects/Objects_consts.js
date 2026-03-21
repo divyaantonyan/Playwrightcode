7ebbb3530380ba6f56ed8cab47b07ecd94c9d7ff
divyaantonyan <divyaantony.an@gmail.com>
2026-03-21 21:26:27 +0530

    objects_consts added

---
+++ chapter11_Objects/Objects_consts.js
@@ -0,0 +1,23 @@
+//objects constants 
+const user = {
+    name: "John",
+    age: 30,
+    email: "john@example.com"
+};
+
+console.log(user);
+
+// Accessing properties
+console.log(user.name);
+console.log(user["age"]); // or console.log(user.age);
+
+// Dynamic property access 
+const key = "email";
+console.log(user[key]);
+
+
+// Adding/modifying properties
+user.city = "NYC";
+user.age = 31;
+
+console.log(user);

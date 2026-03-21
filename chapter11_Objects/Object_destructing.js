409dab5db8092ae779ad6cbdc5a23d948a74799b
divyaantonyan <divyaantony.an@gmail.com>
2026-03-21 21:48:51 +0530

    object destructing  added

---
+++ chapter11_Objects/Object_destructing.js
@@ -0,0 +1,19 @@
+const user = { name1: "John", age: 30, city: "NYC" };
+
+// Basic destructuring means extracting values from an object and assigning them to variables
+const { name1, age } = user;
+console.log(name1);
+console.log(age);
+
+// Rename variables while destructuring 
+const { name1: userName, age: userAge } = user;
+console.log(userName);
+console.log(userAge);
+
+// Default values
+const { country = "USA" } = user;
+console.log(country);
+
+const data = { user: { name: "John", address: { city: "NYC" } } };
+const { user: { address: { city } } } = data;
+console.log(data.user.address.city);
